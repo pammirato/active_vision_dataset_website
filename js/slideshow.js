500e1db0be5c9a0c730e09bfb79f21b6de332376
Keith Whitley <kewhitle@live.unc.edu>
2017-02-14 12:21:52 -0500

    Redesigned Pages

---
+++ js/slideshow.js
@@ -0,0 +1,41 @@
+var slideIndex = 1;
+currentpanel = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
+
+function changePanel(x){
+    var newpanel = "#panel" + x;
+    var previouspanel = "#panel" + currentpanel;
+    $(previouspanel).hide();
+    $(newpanel).show();
+    currentpanel = x;
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("contents").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("contents").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
