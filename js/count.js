e1a2552ecb261b4d1e464b9f2691796803e986a1
Philip Ammirato <ammirato@bluetang.cs.unc.edu>
2018-05-02 11:06:12 -0400

    backup

---
+++ js/count.js
@@ -0,0 +1,10 @@
+
+//https://www.quora.com/What-is-the-best-way-to-count-how-many-times-someone-has-clicked-on-a-link-that-leads-to-another-site
+//function trackOutboundLink(link, category, action) { 
+function trackOutboundLink(link) { 
+try { 
+//_gaq.push(['_trackEvent', category , action]); 
+
+
+} catch(err){}}
+ 
